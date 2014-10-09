3016df33622d1048c30fe06600a1877258567783
malina <arevalomalina@gmail.com>
2014-10-08 23:53:33 -0700

    Revised version returns runtime of all functions within module.

--- FS14_hallway_talk.py
+++ FS14_hallway_talk.py
@@ -1,9 +1,10 @@
-    import time
+import time
+import types
      
-def calculateRunTime(function):
+def calculate_run_time(function):
+    """Decorate a method to time it and print out the time."""
     
     def new_function(*args, **kwargs):
-    
         startTime = time.time()
         result = function(*args, **kwargs)
         duration = time.time() - startTime 
@@ -11,17 +12,43 @@ def calculateRunTime(function):
         return result
 
     return new_function
+
+def time_everything(function):
+    """Decorate every function in the module so that it gets timed."""
+    def new_function(*args, **kwargs):
+        for key, value in globals().items():
+            if type(value) == types.FunctionType:
+                # These two functions don't need to be decorated.  They are part of our APM
+                if key in ('time_everything', 'calculate_run_time'):
+                    continue
+
+                # decorate the function and replace it with the decorated version.
+                timed_function = calculate_run_time(value)
+                globals()[key] = timed_function
+
+        return function(*args, **kwargs)
+
+    return new_function
     
-    
-@calculateRunTime
+
+def do_some_stuff(a, b):
+    """A function that does very little, but calls another, slower function."""
+    some_var = 'this should be very fast'
+    return add_two_numbers_many_times(a, b)
+
+
 def add_two_numbers_many_times(a, b):
-    for i in range(1000000):
+    """This function is pretty slow."""
+    for i in range(5000000):
         total = a + b
     return total
 
 
+# We decorate the main function of our application once, and
+# everything else happens automatically!
+@time_everything
 def main():
-   total = add_two_numbers_many_times(2, 3)
+   total = do_some_stuff(2, 3)
 
 if __name__ == '__main__':
-    main
+    main()
